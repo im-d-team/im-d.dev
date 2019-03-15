6771ac036fd0ce62bc5fde7da5cd1304f1370367
eugene94 <eugenekang94@gmail.com>
2019-03-15 16:16:03 +0900

    :house: Jinseong / test 포스트 작성

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,16 +7,8 @@ import { MarkdownRemark } from "../graphql-types";
 
 import HeaderMenu from "../components/HeaderMenu/HeaderMenu";
 import { withLayout, LayoutProps, menuItems } from "../components/Layout";
-import {
-  Button,
-  Segment,
-  Container,
-  Grid,
-  Header,
-  Icon,
-  Card,
-  Comment,
-} from "semantic-ui-react";
+import { Button, Segment, Container, Grid, Header, Icon, Card, Comment } from "semantic-ui-react";
+import { graphql } from "gatsby";
 
 interface BlogProps extends LayoutProps {
   data: {
@@ -34,24 +26,24 @@ const IndexPage = (props: BlogProps) => {
   const Posts = (
     <Container>
       {posts.map(({ node }: { node: MarkdownRemark }) => {
-        const { frontmatter, timeToRead, fields: { slug }, excerpt } = node;
+        const {
+          frontmatter,
+          timeToRead,
+          fields: { slug },
+          excerpt,
+        } = node;
         const avatar = frontmatter.author.avatar.children[0] as ImageSharp;
         const cover = get(frontmatter, "image.children.0.fixed", {});
 
         const extra = (
           <Comment.Group>
             <Comment>
-              <Comment.Avatar
-                src={avatar.fixed.src}
-                srcSet={avatar.fixed.srcSet}
-              />
+              <Comment.Avatar src={avatar.fixed.src} srcSet={avatar.fixed.srcSet} />
               <Comment.Content>
-                <Comment.Author style={{ fontWeight: 400 }}>
-                  {frontmatter.author.id}
-                </Comment.Author>
+                <Comment.Author style={{ fontWeight: 400 }}>{frontmatter.author.id}</Comment.Author>
                 <Comment.Metadata style={{ margin: 0 }}>
-                  {frontmatter.updatedDate} - {timeToRead} min read
-              </Comment.Metadata>
+                  {frontmatter.createdDate} - {timeToRead} min read
+                </Comment.Metadata>
               </Comment.Content>
             </Comment>
           </Comment.Group>
@@ -61,12 +53,13 @@ const IndexPage = (props: BlogProps) => {
           <Card.Description>
             {excerpt}
             <br />
-            <Link to={slug}>Read moreâ€¦</Link>
+            <Link to={slug}>Read more…</Link>
           </Card.Description>
         );
 
         return (
-          <Card key={slug}
+          <Card
+            key={slug}
             fluid
             image={cover}
             header={frontmatter.title}
@@ -82,13 +75,17 @@ const IndexPage = (props: BlogProps) => {
     <div>
       {/* Master head */}
       <Segment vertical inverted textAlign="center" className="masthead">
-        <HeaderMenu
-          Link={Link} pathname={props.location.pathname} items={menuItems} inverted
-        />
+        <HeaderMenu Link={Link} pathname={props.location.pathname} items={menuItems} inverted />
         <Container text>
-          <Header inverted as="h1">Gatsby 2.0 - Starter kit</Header>
-          <Header inverted as="h2">Typescript - Jest - Semantic UI</Header>
-          <Button primary size="huge">Get started!</Button>
+          <Header inverted as="h1">
+            Gatsby 2.0 - Starter kit
+          </Header>
+          <Header inverted as="h2">
+            Typescript - Jest - Semantic UI
+          </Header>
+          <Button primary size="huge">
+            Get started!
+          </Button>
         </Container>
       </Segment>
 
@@ -98,9 +95,7 @@ const IndexPage = (props: BlogProps) => {
       </Segment>
 
       {/* Key features */}
-      <Segment vertical className="stripe alternate feature">
-
-      </Segment>
+      <Segment vertical className="stripe alternate feature" />
     </div>
   );
 };
@@ -108,53 +103,51 @@ const IndexPage = (props: BlogProps) => {
 export default withLayout(IndexPage);
 
 export const pageQuery = graphql`
-query PageBlog {
-  # Get tags
-  tags: allMarkdownRemark(filter: {frontmatter: {draft: {ne: true}}}) {
-    group(field: frontmatter___tags) {
-      fieldValue
-      totalCount
+  query PageBlog {
+    # Get tags
+    tags: allMarkdownRemark(filter: { frontmatter: { draft: { ne: true } } }) {
+      group(field: frontmatter___tags) {
+        fieldValue
+        totalCount
+      }
     }
-  }
 
-  # Get posts
-  posts: allMarkdownRemark(
-    sort: { order: DESC, fields: [frontmatter___updatedDate] },
-    filter: {
-      frontmatter: { draft: { ne: true } },
-      fileAbsolutePath: { regex: "/blog/" }
-    },
-    limit: 10
-  ) {
-    totalCount
-    edges {
-      node {
-        excerpt
-        timeToRead
-        fields {
-          slug
-        }
-        frontmatter {
-          title
-          updatedDate(formatString: "DD MMMM, YYYY")
-          image {
-          	children {
-              ... on ImageSharp {
-                fixed(width: 700, height: 100) {
-                  src
-                  srcSet
+    # Get posts
+    posts: allMarkdownRemark(
+      sort: { order: DESC, fields: [frontmatter___updatedDate] }
+      filter: { frontmatter: { draft: { ne: true } }, fileAbsolutePath: { regex: "/blog/" } }
+      limit: 10
+    ) {
+      totalCount
+      edges {
+        node {
+          excerpt
+          timeToRead
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+            updatedDate(formatString: "DD MMMM, YYYY")
+            image {
+              children {
+                ... on ImageSharp {
+                  fixed(width: 700, height: 100) {
+                    src
+                    srcSet
+                  }
                 }
               }
             }
-          }
-          author {
-            id
-            avatar {
-              children {
-                ... on ImageSharp {
-                  fixed(width: 35, height: 35) {
-                    src
-                    srcSet
+            author {
+              id
+              avatar {
+                children {
+                  ... on ImageSharp {
+                    fixed(width: 35, height: 35) {
+                      src
+                      srcSet
+                    }
                   }
                 }
               }
@@ -164,5 +157,4 @@ query PageBlog {
       }
     }
   }
-}
 `;
