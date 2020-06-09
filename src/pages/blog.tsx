e255b33798aa544094e7b474719f8b9518020845
SeonHyungJo <seonhyung.jo@gmail.com>
2020-06-10 01:14:13 +0900

    :pencil2: Fix : 빌드안되는 이슈 해결

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -36,3 +36,54 @@ const BlogPage = ({ data, location }: BlogProps) => {
 };
 
 export default withLayout(BlogPage);
+
+export const pageQuery = graphql`
+  query PageBlog2 {
+    # Get tags
+    tags: allMarkdownRemark(filter: { frontmatter: { draft: { ne: true } } }) {
+      group(field: frontmatter___tags) {
+        fieldValue
+        totalCount
+      }
+    }
+
+    # Get posts
+    posts: allMarkdownRemark(
+      sort: { order: DESC, fields: [frontmatter___updatedDate] }
+      filter: {
+        frontmatter: { draft: { ne: true } }
+        fileAbsolutePath: { regex: "/blog/" }
+      }
+      limit: 5
+    ) {
+      totalCount
+      edges {
+        node {
+          excerpt(pruneLength: 100, truncate: true)
+          timeToRead
+          fields {
+            slug
+          }
+          frontmatter {
+            title
+            updatedDate(formatString: "MMMM DD, YYYY")
+            createdDate(formatString: "MMMM DD, YYYY")
+            tags
+            author {
+              id
+              github
+              avatar {
+                childImageSharp {
+                  fixed(width: 35, height: 35) {
+                    src
+                    srcSet
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
